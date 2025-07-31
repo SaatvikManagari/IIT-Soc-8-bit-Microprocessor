5de5a129db35aed95e478587771b0a2b21ef5d7a
Abhi1517621 <ee240002041@iiti.ac.in>
2025-07-31 18:11:28 +0530

    Update support_files.v

--- mid_evaluation/Mid_eval.srcs/sources_1/new/support_files.v
+++ mid_evaluation/Mid_eval.srcs/sources_1/new/support_files.v
@@ -1,25 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 06/20/2025 06:52:49 PM
-// Design Name: 
-// Module Name: support_files
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
-// Single unit bit memory
 
 module d_flip_flop(
     input D,clk,
